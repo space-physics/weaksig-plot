c73b1ce0f190bd9f9075153187e2d2b2ccd1a6b2
scivision <scivision@users.noreply.github.com>
2017-02-23 14:44:54 -0500

    doc

--- setup.py
+++ setup.py
@@ -7,5 +7,13 @@ req = ['nose','numpy','pandas','matplotlib','seaborn']
 setup(name='weaksig_plot',
       packages=['weaksig_plot'],
       author='Michael Hirsch, Ph.D.',
+      url='https://github.com/scienceopen/weaksig-plot',
+      classifiers=[
+      'Intended Audience :: Science/Research',
+      'Development Status :: 3 - Alpha',
+      'License :: OSI Approved :: GNU Affero License',
+      'Topic :: Scientific/Engineering :: Atmospheric Science',
+      'Programming Language :: Python :: 3.6',
+      ],
       install_requires=req,
 	  )
