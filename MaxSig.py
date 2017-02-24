f3e6355b32dec77d1d109b230b01ccfe4849cc5c
scivision <scivision@users.noreply.github.com>
2017-02-23 21:22:28 -0500

    cmd line api

--- MaxSig.py
+++ MaxSig.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python
 """
 http://wsprnet.org/drupal/downloads
+
+Processes and plot gigabyte .csv files containing WSPR propagation data
 """
+from pandas import DataFrame
 from matplotlib.pyplot import show
 #
 from weaksig_plot import readwspr, wsprstrip,plottime
 
-fn = 'data/wsprspots-2017-02.csv'
-fn = 'data/2017-02-23.tsv'
-callsign = 'W1BUR'
-freq = [3,7] # 3, 7 [MHz]
+#fn = 'data/wsprspots-2017-02.csv'
+#fn = 'data/2017-02-23.tsv'
 
-def wsprplots(dat,callsign,freq):
-    for f in freq:
-        wsprstrip(dat,callsign, f)
+def wsprplots(dat:DataFrame, callsign:str, band:int):
+    for b in band:
+        wsprstrip(dat,callsign, b)
 
     #plottime(dat,callsign,freq[1])
 
 if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser(description='WSPR weak signal SNR/Hz/Watt plotter')
+    p.add_argument('callsign',help='callsign of interest (case insensitive)')
+    p.add_argument('fn',help='directory or list of .csv or .tsv files to plot')
+    p.add_argument('-b','--band',help='frequency band (integer MHz) to plot [3,5,7]',nargs='+',default=[3,5,7],type=int)
+    p = p.parse_args()
 
-    try:
-        if len(dat)==0:
+    try: # save time by reusing already loaded data
+        if dat.shape[0]==0: # bad read, try again
             del dat
-        wsprplots(dat,callsign,freq)
-    except NameError:
-        dat = readwspr(fn, callsign, freq)
-        wsprplots(dat,callsign,freq)
+        wsprplots(dat, p.callsign, p.band)
+    except NameError: # load then plot
+        dat = readwspr(p.fn, p.callsign, p.band)
+        wsprplots(dat, p.callsign, p.band)
 
     show()
--- README.rst
+++ README.rst
@@ -3,6 +3,9 @@ weaksig-plot
 ============
 
 WSPR, JT65, JT9 and other weak signal modes: read, analyze, plot data. Minimalistic.
+Right now, the emphasis is on expressing expected ranges of SNR/Hz/Watt, binned by range (groundwave, NVIS, DX) and local time for each frequency band (3 MHz, 5 MHz, etc.).
+This helps understand what is feasible for general NVIS communications systems referenced to narrowband weak signal modes such as WSPR.
+
 
 
 Install
@@ -20,4 +23,12 @@ program  description
 ======== ===================
 MaxSig   plots maximum signal on a frequency vs. distance "What's the strongest I'm heard at a distance and frequency?"
 ======== ===================
+
+Data Formats
+============
+The ``.csv`` format is what's inside `each monthly WSPR log file <http://wsprnet.org/drupal/downloads>`_.
+The ``.tsv`` format is from copying and pasting (from your web browser) a `database query result <http://wsprnet.org/olddb>`_.
+
+The programs automatically decide based on the file extension how to decode the file.
+
 
