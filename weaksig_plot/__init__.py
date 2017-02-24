a8fc3c0e494b353e25048b3c4501746fd2dbbc35
scivision <scivision@users.noreply.github.com>
2017-02-24 17:02:41 -0500

    safer time handling

--- weaksig_plot/__init__.py
+++ weaksig_plot/__init__.py
@@ -12,6 +12,7 @@ refbw = 2500 #[Hz] for WSPR
 refdbm = 30 # [dBm] makes SNR/W
 MAXNVISDIST=250 # [km] arbitrary maximum distance to be considered NVIS for plotting time
 MINPOINTS = 3 # minimum number of station measurements to be time plotted
+TIMEZONE = 'US/Eastern' # TODO make parameter
 
 def readwspr(fn, callsign:str, band:int) -> DataFrame:
     fn = Path(fn).expanduser()
@@ -74,7 +75,7 @@ def wsprstrip(dat:DataFrame, callsign:str, band:int):
 
     cats = cut(dat['distkm'], bins)
     #dat['range_bins'] = cats
-    dat,hcats = cathour(dat)
+    dat,hcats = cathour(dat, TIMEZONE)
 #%% swarm
     ax = figure().gca()
     sns.swarmplot(x=cats,y='snr', hue=hcats, data=dat, ax=ax)
@@ -111,7 +112,7 @@ def plottime(dat:DataFrame, callsign:str, band:int, maxcalls:int):
         if cdat.shape[0] < MINPOINTS or distkm>MAXNVISDIST: # if not many data points, skip plotting, or if too far for NVIS
             continue
 
-        cdat, hcats = cathour(cdat)
+        cdat, hcats = cathour(cdat, TIMEZONE)
 #%%
         ax = figure().gca()
         sns.swarmplot(x=hcats,y='snr',hue=hcats,data=cdat,ax=ax)
@@ -119,9 +120,10 @@ def plottime(dat:DataFrame, callsign:str, band:int, maxcalls:int):
         ax.set_ylabel('SNR/Hz/W [dB]')
 #%%
 
-def cathour(dat): 
-    tlocal = [t.astimezone(timezone('US/Eastern')) for t in dat['t']]
-    dat['hod'] = [r.hour for r in tlocal]
+def cathour(dat, tz): 
+    # hour of day, local time
+    dat['hod'] = [t.astimezone(timezone(tz)).hour for t in dat['t']]
+
     bins = range(0,24+6,6) # hours of day
     cats = cut(dat['hod'],bins)
 
