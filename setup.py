cd1d647f42271b3aaa9a9e5e16fb9d8338f031d3
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-10-26 15:00:56 -0400

    template

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
 #!/usr/bin/env python
-req = ['nose','numpy','pandas','matplotlib','seaborn','basemap']
-pipreq=['sciencedates','pymap3d','maidenhead',]
-
-# %%
 from setuptools import setup
-
-setup(name='weaksig_plot',
-      packages=['weaksig_plot'],
-      author = 'Michael Hirsch, Ph.D.',
-      url = 'https://github.com/scivision/weaksig-plot',
-      classifiers=[
-      'Intended Audience :: Science/Research',
-      'Development Status :: 3 - Alpha',
-      'License :: OSI Approved :: GNU Affero License',
-      'Topic :: Scientific/Engineering :: Atmospheric Science',
-      'Programming Language :: Python :: 3',
-      ],
-      install_requires=req+pipreq,
-#      dependency_links=['https://downloads.sourceforge.net/project/matplotlib/matplotlib-toolkits/basemap-1.0.7/basemap-1.0.7.tar.gz'],
-	  )
+setup()
